24c4c8be541a4e9eeb47f7ae4d740029fa6b87cc
Carlos Macias <cimm21187@gmail.com>
2023-08-15 22:30:47 -0600

    Implement functionality to fetch weather data.

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+var apiKey = '97fa0144a21139182aed6c24b1b8ee86';
+
+$("#currentDay").text(new Date().toDateString());
+
+function getWeatherData(cityName) {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=metric&appid=' + apiKey)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        console.log("Current weather data:", data);
+        return fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&units=metric&appid=' + apiKey);
+    })
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        console.log("5-day forecast data:", data);
+    })
+    .catch(function(error) {
+        console.error("There was an error fetching the weather data.", error);
+    });
+}
